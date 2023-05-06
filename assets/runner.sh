08985d56f3b58b35755219b455a2a470e371dd74
wjallen <wallen@tacc.utexas.edu>
2023-05-06 02:32:06 -0500

    fixing conditional

--- assets/runner.sh
+++ assets/runner.sh
@@ -18,20 +18,20 @@ then
 fi
 
 # mpi settings
-PREFIX_MPI_SETTINGS=""
-if [ $library == *"Test-set"* ]; then
-    PREFIX_MPI_SETTINGS="MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=0 "
-elif [ $library == *"Enamine-PC"* ]; then
-    PREFIX_MPI_SETTINGS="MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=0 "
-elif [ $library == *"Enamine-AC"* ]; then
-    PREFIX_MPI_SETTINGS="MV2_ENABLE_AFFINITY=0 "
-elif [ $library == *"Enamine-HTSC"* ]; then
-    PREFIX_MPI_SETTINGS="MV2_ENABLE_AFFINITY=0 "
-elif [ $library == *"ZINC-fragments"* ]; then
-    PREFIX_MPI_SETTINGS="MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=0 "
-elif [ $library == *"ZINC-in-trials"* ]; then
-    PREFIX_MPI_SETTINGS="MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=0 "
-fi
+MPI_SETTING=""
+if [ "${library}" = "/scratch/projects/docking/Test-set-compressed" ]; then         
+    MPI_SETTING="0"               
+elif [ "${library}" = "/scratch/projects/docking/Enamine-PC-compressed" ]; then     
+    MPI_SETTING="0"               
+elif [ "${library}" = "/scratch/projects/docking/Enamine-AC-compressed" ]; then     
+    MPI_SETTING="1"                                 
+elif [ "${library}" = "/scratch/projects/docking/Enamine-HTSC-compressed" ]; then   
+    MPI_SETTING="1"                                 
+elif [ "${library}" = "/scratch/projects/docking/ZINC-fragments-compressed" ]; then 
+    MPI_SETTING="0"               
+elif [ "${library}" = "/scratch/projects/docking/ZINC-in-trials-compressed" ]; then 
+    MPI_SETTING="0"               
+fi      
 
 
 # Log commands, timing, run job
@@ -39,8 +39,8 @@ echo -n "starting: "
 date
 
 echo "================================================================"
-echo "${PREFIX_MPI_SETTINGS} ibrun apptainer exec vina_1.2.3.sif \ "
-echo "    python3 autodock.py \ "
+echo "MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=${MPI_SETTING} ibrun apptainer exec vina_1.2.3.sif \ "
+echo "    python3 /autodock-src/autodock.py \ "
 echo "    -r ${receptor} \ "
 echo "    --center="${center_x},${center_y},${center_z}" \ "
 echo "    -s "${size_x},${size_y},${size_z}" \ "
@@ -51,8 +51,8 @@ echo "    -n ${top_n_scores} \ "
 echo "    -f ${FLEX} "
 echo "================================================================"
 
-MV2_ENABLE_AFFINITY=0 ibrun apptainer exec vina_1.2.3.sif \
-     python3 autodock.py \
+MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=${MPI_SETTING} ibrun apptainer exec vina_1.2.3.sif \
+     python3 /autodock-src/autodock.py \
      -r ${receptor} \
      --center="${center_x},${center_y},${center_z}" \
      -s "${size_x},${size_y},${size_z}" \
@@ -64,4 +64,7 @@ MV2_ENABLE_AFFINITY=0 ibrun apptainer exec vina_1.2.3.sif \
 
 echo -n "ending: "
 date
+
+# clean up
+rm vina_1.2.3.sif
 
