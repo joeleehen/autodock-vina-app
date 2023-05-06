79f70e97e23878ec6d986879d3235109931ad775
wjallen <wallen@tacc.utexas.edu>
2023-05-06 11:32:15 -0500

    taking out mv2_smp_use_cma for now

--- assets/runner.sh
+++ assets/runner.sh
@@ -4,10 +4,7 @@ then
     CONTAINER_IMAGE="wjallen/autodock_vina:1.2.3"
 fi
 
-# Silence xalt errors
-module unload xalt
 apptainer pull --disable-cache vina_1.2.3.sif docker://${CONTAINER_IMAGE}
-
 
 # set flexible sidechains, if any
 FLEX=${flexible_sidechains}
@@ -17,21 +14,13 @@ then
     FLEX='Empty'
 fi
 
-# mpi settings
-MPI_SETTING=""
-if [ "${library}" = "/scratch/projects/docking/Test-set-compressed" ]; then         
-    MPI_SETTING="0"               
-elif [ "${library}" = "/scratch/projects/docking/Enamine-PC-compressed" ]; then     
-    MPI_SETTING="0"               
-elif [ "${library}" = "/scratch/projects/docking/Enamine-AC-compressed" ]; then     
-    MPI_SETTING="1"                                 
-elif [ "${library}" = "/scratch/projects/docking/Enamine-HTSC-compressed" ]; then   
-    MPI_SETTING="1"                                 
-elif [ "${library}" = "/scratch/projects/docking/ZINC-fragments-compressed" ]; then 
-    MPI_SETTING="0"               
-elif [ "${library}" = "/scratch/projects/docking/ZINC-in-trials-compressed" ]; then 
-    MPI_SETTING="0"               
-fi      
+# mpi settings - turn this on for multi node jobs
+VAL="0"
+# if [ "${library}" = "/scratch/projects/docking/Enamine-AC-compressed" ]; then
+#     VAL="1"
+# elif [ "${library}" = "/scratch/projects/docking/Enamine-HTSC-compressed" ]; then
+#     VAL="1"
+# fi
 
 
 # Log commands, timing, run job
@@ -39,7 +28,7 @@ echo -n "starting: "
 date
 
 echo "================================================================"
-echo "MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=${MPI_SETTING} ibrun apptainer exec vina_1.2.3.sif \ "
+echo "MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=${VAL} ibrun apptainer exec vina_1.2.3.sif \ "
 echo "    python3 /autodock-src/autodock.py \ "
 echo "    -r ${receptor} \ "
 echo "    --center="${center_x},${center_y},${center_z}" \ "
@@ -51,7 +40,7 @@ echo "    -n ${top_n_scores} \ "
 echo "    -f ${FLEX} "
 echo "================================================================"
 
-MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=${MPI_SETTING} ibrun apptainer exec vina_1.2.3.sif \
+MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=${VAL} ibrun apptainer exec vina_1.2.3.sif \
      python3 /autodock-src/autodock.py \
      -r ${receptor} \
      --center="${center_x},${center_y},${center_z}" \
