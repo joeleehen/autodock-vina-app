8c8eaa83ed594fdfa87c679c2b984e1fe42c147a
wjallen <wallen@tacc.utexas.edu>
2024-07-10 11:44:34 -0500

    always setting MV2_SMP_USE_CMA to 0

--- run.sh
+++ run.sh
@@ -16,13 +16,6 @@ receptor=$(find * -type f | grep -v 'tapisjob')
 #    FLEX='Empty'
 #fi
 
-# mpi settings - set VAL based on number of nodes
-if [[ "${_tapisNodes}" > 1 ]]; then
-    VAL=1
-else 
-    VAL=0
-fi
-
 
 # Log commands, timing, run job
 echo -n "starting: "
@@ -33,14 +26,14 @@ echo "python3 /autodock-src/autodock.py \\"
 echo "    --receptor ${receptor} \\"
 echo "    --ligand_library ${ligand_library} \\"
 echo "    --center=\"${box_center}\" \\"
-echo "    --size \"${box_size}}\" \\"
+echo "    --size \"${box_size}\" \\"
 echo "    --number ${top_n_scores} \\"
 echo "    --forcefield ${force_field} \\"
 echo "    --docking ${docking_method} \\"
 echo "    --sidechains ${flexible_sidechains} "
 echo "================================================================"
 
-MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=${VAL} \
+MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=0 \
 python3 /autodock-src/autodock.py \
      --receptor ${receptor} \
      --ligand_library ${ligand_library} \
